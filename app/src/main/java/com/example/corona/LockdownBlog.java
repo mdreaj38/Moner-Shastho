ece22c6d674274e9b5547b18c38ffa3a491dc63f
mdreaj38 <mdreaj38@gmail.com>
2020-05-20 01:58:57 +0600

    LockDown done+update profile is updated

---
+++ app/src/main/java/com/example/corona/LockdownBlog.java
@@ -0,0 +1,14 @@
+package com.example.corona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LockdownBlog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lockdown_blog);
+    }
+}
